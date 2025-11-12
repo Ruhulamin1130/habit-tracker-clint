881f3da2e724fcce969d2eb631cbac402f983c5e
Ruhul Amin <ruhulayahoo@gmail.com>
2025-11-12 07:20:48 -0800

    sequire fibase config file

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAns_8QDv7kl_aP6ZOso7nAA-GTs_Mgxgs",
-  authDomain: "habit-tacker-9a43a.firebaseapp.com",
-  projectId: "habit-tacker-9a43a",
-  storageBucket: "habit-tacker-9a43a.firebasestorage.app",
-  messagingSenderId: "170485839295",
-  appId: "1:170485839295:web:9701cccce1c61af4c85d0a",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export default app;
